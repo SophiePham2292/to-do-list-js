17c6c86a62d6ccc92dc16563e0ada394373d4c92
Trang <choe.2292@gmail.com>
2018-10-31 19:23:50 +0700

    Change list based on piority

--- to-do-list.js
+++ to-do-list.js
@@ -1,9 +1,8 @@
 const data = [
-    { title: 'Task 1', done: false },
-    { title: 'Task 2', done: false },
-    { title: 'Task 3', done: false }
+    { title: 'Task 1', done: false, piority: 3 },
+    { title: 'Task 2', done: false, piority: 3 },
+    { title: 'Task 3', done: false, piority: 3 }
 ]
-
 
 
 class ToDoList {
@@ -21,7 +20,8 @@ class ToDoList {
         const taskContent = this.el.getElementsByClassName('task-input')[0].value;
         data.push({
             title: taskContent,
-            done: false
+            done: false,
+            piority: 3
         }) 
         const taskList = this.el.getElementsByClassName('task-list')[0];
         while (taskList.firstChild) taskList.removeChild(taskList.firstChild);
@@ -29,12 +29,7 @@ class ToDoList {
         listRender()      
     }
 
-    handleDeleteTask(e){
-        e.preventDefault();
-        console.log(e.target);
-        const button = e.target;
-        console.log(button.parentElement);
-    }
+   
 }
 
 ToDoList.template = `
@@ -57,24 +52,47 @@ ToDoList.template = `
 new ToDoList('to-do-list');
 
 const list = document.getElementsByClassName('task-list')[0];
-console.log(list);
 function listRender() {
     while(list.firstChild) list.removeChild(list.firstChild);
     return (
         data.map((item, index) => {
-            const $item = document.createElement('li')
+            const $item = document.createElement('li');
             $item.innerHTML = `
                 <input type="checkbox" name="task-done">
                 <div></div>
                 <select name="task-piority" id="task-piority">
-                    <option value="high">High</option>
-                    <option value="medium">Medium</option>
                     <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
                 </select>
                 <button class='delete-task'>Delete</button>
             `;
             $item.classList.add('task-detail');
             $item.getElementsByTagName('div')[0].innerText = item.title;
+
+            // Change selected option based on data piority 
+
+            switch(data[index].piority) {
+                case 1:
+                    $item.getElementsByTagName('option')[2].setAttribute('selected', 'true');
+                    break;
+                case 2:
+                    $item.getElementsByTagName('option')[1].setAttribute('selected', 'true');
+                    break;
+                default:
+                    $item.getElementsByTagName('option')[0].setAttribute('selected', 'true')
+            }
+
+           /*  if(data[index].piority === 1) {
+                $item.getElementsByTagName('option')[2].setAttribute('selected', 'true')
+            }
+            if(data[index].piority === 2) {
+                $item.getElementsByTagName('option')[1].setAttribute('selected', 'true')
+            }
+            if(data[index].piority === 3) {
+                $item.getElementsByTagName('option')[0].setAttribute('selected', 'true')
+            } */
+
             list.appendChild($item);
 
             $item.getElementsByTagName('button')[0].addEventListener('click', function(e){
@@ -82,6 +100,34 @@ function listRender() {
                 data.splice(index, 1);
                 listRender();
             })
+
+            $item.getElementsByTagName("select")[0].addEventListener('change', function(e) {
+                console.log(e.target.getElementsByTagName('option'));
+                    switch(e.target.value){
+                        case 'high':
+                            data[index].piority = 1;
+                            break;
+                        case 'medium':
+                            data[index].piority = 2;
+                            break;
+                        case 'low':
+                            data[index].piority = 3;
+                            break;
+                    }
+
+                    function comparePiority(item1, item2) {
+                        if(item1.piority > item2.piority){                    
+                            return 1;
+                        } else if (item1.piority < item2.piority){
+                            return -1;
+                        }
+                        return 0;
+                    }
+                    data.sort(comparePiority);
+
+                    listRender();
+            })
+
         }))
 }
 
